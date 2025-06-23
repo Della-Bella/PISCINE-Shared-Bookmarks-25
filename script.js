450de5a4779e7eac0d2364f9730af599e15753dc
Della Bella <gisdellabella@uol.com.br>
2025-06-23 21:15:55 +0100

    Set UP HTML struture

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
+
+// Dropdonw Elements:
+
+const userIdDropdonw = document.getElementById("user-dropdonw");
+const userSelected = document.getElementById("user-select");
+
+
+//Form Elements
+
+const bookmarkForm = document.getElementById("bookmark-form");
+const titleInput = document.getElementById("title-input");
+const urlInput = document.getElementsById("url-input");
+const descriptionInput = document.getElementById("description-input");
+
+// Display Area
+
+const bookmarksList = document.getElementById("bookmarks-list"); 
+
+
 
 import { getUserIds } from "./storage.js";
 
 window.onload = function () {
-  const users = getUserIds();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
+   const users = getUserIds();
 };
