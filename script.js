495a7ea020de36feda8c43725c765ba0c3d11658
Della Bella <gisdellabella@uol.com.br>
2025-06-23 22:05:49 +0100

    Add Options for the Dropdonw

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
+import { getUserIds } from "./storage.js";
 
 // Dropdonw Elements:
-
-const userIdDropdonw = document.getElementById("user-dropdonw");
 const userSelected = document.getElementById("user-select");
 
-
 //Form Elements
-
 const bookmarkForm = document.getElementById("bookmark-form");
 const titleInput = document.getElementById("title-input");
-const urlInput = document.getElementsById("url-input");
+const urlInput = document.getElementById("url-input");
 const descriptionInput = document.getElementById("description-input");
 
 // Display Area
+const bookmarksList = document.getElementById("bookmarks-list");
 
-const bookmarksList = document.getElementById("bookmarks-list"); 
+   // --- Function Populate Dropdonw ---
+   function populateUserDropdown() {
+      // 1. Get user IDs from storage
+      const userIds = getUserIds();
 
+      // 2. Loop through each userId in the array
+      userIds.forEach((userId) => {
+         // 3. Create a new <option> element
+         const option = document.createElement("option");// create otios names users
 
+         // 4. Set value and text
+         option.value = userId;// find user
+         option.textContent = userId;// names
 
-import { getUserIds } from "./storage.js";
-
-window.onload = function () {
-   const users = getUserIds();
-};
+         // 5. Append the new option to the <select> dropdown
+         userSelected.appendChild(option);
+         console.log ("dropdonw-working")
+      });
+   }
+      
+// Call the function to run the code when the page loads
+populateUserDropdown();
