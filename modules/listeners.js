6625d2aeb13c79b1a3c373916bc4b0fef92073ed
Della Bella <gisdellabella@uol.com.br>
2025-06-25 08:18:45 +0100

    First Step of subimt: get values  and create new object for the new bookmarks

--- modules/listeners.js
+++ modules/listeners.js
@@ -1,10 +1,18 @@
 //    Issue #3 Handle User Selection Change/ Event Listeners/  function call.
 
-import { userSelected } from "./dom.js";
-import { getData } from "./storage.js";
+import {
+   userSelected,
+   bookmarkForm,
+   titleInput,
+   urlInput,
+   descriptionInput,
+} from "./dom.js";
+import { getData, setData } from "./storage.js";
 import { renderBookmarks } from "./bookmarks.js";
+ 
 
 export function setupEventListeners() {
+   // LISTENER 1: For the Dropdown
    userSelected.addEventListener("change", (event) => {
       const selectedUserId = event.target.value; // gte the user iD
 
@@ -12,7 +20,39 @@ export function setupEventListeners() {
       console.log("5. New user selected:", selectedUserId);
 
       const bookmarks = getData(selectedUserId); // get the usser data
-      console.log("6. Fetched new bookmarks:", bookmarks); 
+      console.log("6. Fetched new bookmarks:", bookmarks);
       renderBookmarks(bookmarks); // render data brenderBookmamrk Function
+   });
+
+   
+   // Start submit ____LISTENER 2: For the Form Submission
+   // Start submit
+   //1= First thing is to get all the information we need for submit the firm
+   //Inside the submit event listener, we will read the .value property of each
+   //  of these elements and store them in constants.
+   bookmarkForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const currentUserId = userSelected.value;
+      const title = titleInput.value;
+      const url = urlInput.value;
+      const description = descriptionInput.value;
+
+      //2= CREATE THE BOOKMARK OBJECT 
+      const newBookmark = {
+         title: title,
+         url: url,
+         description: description,
+         createdAt: new Date().toISOString(), 
+      };
+
+      //check 
+      console.log("Created new bookmark object:", newBookmark);
+
+
+      // Check 
+      console.log("Form submitted for user:", currentUserId);
+      console.log("Title entered:", title);
+      console.log("URL entered:", url);
+      console.log("Description entered:", description);
    });
 }
